be39ea86760e2d7d0b4c920418bec9de61252efc
Li, Yifei 李逸飞 <realliyifei@outlook.com>
2022-01-20 01:28:38 -0500

    show citation

--- WikipediaOptimizor.user.js
+++ WikipediaOptimizor.user.js
@@ -3,17 +3,23 @@
 // @version       Beta
 // @description	  A neat warm paper theme, with smart toc sidebar, explicit multilingual links, and other powerful optimizations
 // @license       MIT
-// @author        Li, Yi-Fei
-// @namespace     https://github.com/realliyifei/Userscript-Wikipedia-Optimizor
+// @author        Li, Yifei
+// @namespace     https://github.com/realliyifei/userscript-wikipedia-optimizor
 // @include       http://wikipedia.org/*
 // @include       https://wikipedia.org/*
 // @include       http://*.wikipedia.org/*
 // @include       https://*.wikipedia.org/*
 // @include       http://en.wikipedia.org/wiki/File:*
 // @run-at        document-start
-// @downloadURL   https://github.com/realliyifei/Userscript-Wikipedia-Optimizor/blob/master/WikipediaOptimizor.user.js
-// @updateURL     https://github.com/realliyifei/Userscript-Wikipedia-Optimizor/blob/master/WikipediaOptimizor.user.js
-// @supportURL    https://github.com/realliyifei/Userscript-Wikipedia-Optimizor/issues
+// @downloadURL   https://github.com/realliyifei/userscript-wikipedia-optimizor/blob/master/WikipediaOptimizor.user.js
+// @updateURL     https://github.com/realliyifei/userscript-wikipedia-optimizor/blob/master/WikipediaOptimizor.user.js
+// @supportURL    https://github.com/realliyifei/userscript-wikipedia-optimizor/issues
+// Notes: This require is for "LANGUAGE DISPLAY"
+// @require       https://code.jquery.com/jquery-3.2.1.min.js
+// Notes: This three requires are for "STICK TABLE HEADERS"
+// @require       https://code.jquery.com/jquery-1.12.0.min.js
+// @require       https://greasyfork.org/scripts/2199-waitforkeyelements/code/waitForKeyElements.js?version=6349
+// @require       https://cdnjs.cloudflare.com/ajax/libs/floatthead/1.3.2/jquery.floatThead.min.js
 // ==/UserScript==
 
 /* WARM PAPER THEME */
@@ -90,7 +96,7 @@ var css = `
     .noprint {display: none!important;}
 
     #panel, #head,#page-base
-        { display: none !important; }
+      { display: none !important; }
 
 
     /* section headers styple */
@@ -289,10 +295,10 @@ var css = `
     table#mw-search-top-table tr td input {display:none!important;}
 
     /* hide citations */
-    a[href=\"#References\"], sup[class=\"reference\"], span[id=\"References\"], ol[class=\"references\"], .refbegin {display:none !important;}
+    // a[href=\"#References\"], sup[class=\"reference\"], span[id=\"References\"], ol[class=\"references\"], .refbegin {display:none !important;}
 
     /* hide rating */
-    div#mw-articlefeedback {display:none!important;}
+    // div#mw-articlefeedback {display:none!important;}
 
     /* hide coordinates on location pages */
     #coordinates {display:none !important;}
@@ -331,46 +337,46 @@ css = `
     /* hide image properties box on image page */
     ul#filetoc {display:none !important;}
     h1#firstHeading.firstHeading {display:none !important;}
-    div.sharedUploadNotice {display:none !important;}
-    div#file.fullImageLink {text-align:center !important;}
+	div.sharedUploadNotice {display:none !important;}
+	div#file.fullImageLink {text-align:center !important;}
 `;
 
 if (typeof GM_addStyle != "undefined") {
-    GM_addStyle(css);
+	GM_addStyle(css);
 } else if (typeof PRO_addStyle != "undefined") {
-    PRO_addStyle(css);
+	PRO_addStyle(css);
 } else if (typeof addStyle != "undefined") {
-    addStyle(css);
+	addStyle(css);
 } else {
-    var node = document.createElement("style");
-    node.type = "text/css";
-    node.appendChild(document.createTextNode(css));
-    var heads = document.getElementsByTagName("head");
-    if (heads.length > 0) {
-        heads[0].appendChild(node);
-    } else {
-        // no head yet, stick it whereever
-        document.documentElement.appendChild(node);
-    }
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
 }
 })();
 
 /* CONTENTS ON SCROLL */
 window.onscroll = function() {
-    var t = document.documentElement.scrollTop || document.querySelector('#content').scrollTop; // Get the current height
-    // Get the point of aim
-    var menu = document.querySelectorAll('div#toc>ul a');
-    var flag = -1;
-    for (var i = 0; i < menu.length; i++) {
-        var ssid = menu[i].href.split('#');
-        ssid = ssid[ssid.length - 1];
+   var t = document.documentElement.scrollTop || document.querySelector('#content').scrollTop; // Get the current height
+   // Get the point of aim
+   var menu = document.querySelectorAll('div#toc>ul a');
+   var flag = -1;
+   for (var i = 0; i < menu.length; i++) {
+      var ssid = menu[i].href.split('#');
+      ssid = ssid[ssid.length - 1];
 
-        if (document.getElementById(ssid).offsetTop + 200 >= t) {
-            // menu[i].closest('li').focus();
-            flag = i;
-            break;
-        }
-    }
+      if (document.getElementById(ssid).offsetTop + 200 >= t) {
+         // menu[i].closest('li').focus();
+         flag = i;
+         break;
+      }
+   }
 
     for (var ik = 0; ik < menu.length; ik++) {
         if (flag == ik) { // find the corresponding title
@@ -388,3 +394,87 @@ window.onscroll = function() {
 
 };
 
+/* LANGUAGE DISPLAY */
+(function() {
+    var langs = ['en','zh','fr']; // Add language tabs, e.g. fr, de, ja, ko, etc. (ISO 639-1)
+    for(var i in langs){
+        var lang = $('#p-lang > div > ul > li.interlanguage-link.interwiki-' + langs[i]).html();
+        console.log('F: '+lang);
+        if(lang){
+            var node = '<li><span>' + lang + '</span></li>';
+            // $('#mw-page-base').append(node)
+            // $('#mw-head-base').append(node)
+            // $('#content').append(node)
+            // $('#toc').append(node)
+            // $('#mw-page-base > div > ul').append(node)
+            // $('#toc > div > ul').append(node);
+            $('#toc > div > ul').append(node)
+            /*
+            var button = document.createElement("Button");
+            // button.innerHTML = "Title";
+            button.innerHTML = lang;
+            button.style = "top:0; right:0;position:fixed;"
+            document.body.appendChild(button);
+            */
+        }
+    }
+})();
+
+(function () {
+    /*
+        Customize labels by editing LABEL_STYLE.
+        "LABEL", "LANG_CODE" and "LANG_NAME" will be replaced.
+
+        e.g.
+            "LABEL (LANG_NAME)" => "Étoile (Français)"
+            "[LANG_CODE]LABEL" => "[fr]Étoile"
+     */
+    var LABEL_STYLE = 'Étoile (Français)';
+
+    var _REPLACEMENT_LABEL = 'LABEL';
+    var _REPLACEMENT_LANG_CODE = 'LANG_CODE';
+    var _REPLACEMENT_LANG_NAME = 'LANG_NAME';
+    var _REGEXP_LINK = new RegExp('http:\/\/([a-z\-]+)\.wikipedia\.org\/wiki\/([^#]+)');
+    var _REGEXP_SPACES = new RegExp('_', 'g');
+    var links = document.getElementById('p-lang').getElementsByTagName('A');
+    for (var i=0; i<links.length; i++) {
+        var link = links[i];
+        if (link.hreflang && link.href.match(_REGEXP_LINK)) {
+            var langCode = RegExp.$1;
+            var langName = link.innerHTML;
+            var label = decodeURI(RegExp.$2).replace(_REGEXP_SPACES, ' ');
+            link.innerHTML = LABEL_STYLE
+                .replace(_REPLACEMENT_LABEL, label)
+                .replace(_REPLACEMENT_LANG_CODE, langCode)
+                .replace(_REPLACEMENT_LANG_NAME, langName);
+            // START LYF
+
+            // var button = document.createElement(langName);
+            var button = document.createElement("test");
+            button.innerHTML = link.innerHTML;
+            button.style = "top:0;right:0;position:fixed;"
+            document.body.appendChild(button);
+
+            //$('button selector').click(function(){
+            //   window.location.href=link.innerHTML;
+            //})
+            // END LYF
+        }
+    }
+})();
+
+/*
+var button = document.createElement("Button");
+button.innerHTML = "Title";
+button.style = "top:0;right:0;position:absolute;z-index: 9999"
+document.body.appendChild(button);
+*/
+/* STICK TABLE HEADERS (bug) */
+waitForKeyElements ("table.jquery-tablesorter", floatTableHeaders, true);
+
+function floatTableHeaders(table){
+  $(table).floatThead();
+}
+
+/* ClEAN DONATION REQUEST */
+// GM.addStyle("html body #centralNotice, html body [id*=frbanner], html body [id*=frb-inline] { display: none !important; }");
